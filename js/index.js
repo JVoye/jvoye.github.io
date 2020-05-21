3cc8d3aa9935a0c36b51ddc6c657a85ed3cd1a0c
Justin Voye <justin_voye@yahoo.com>
2020-05-21 09:47:21 -0600

    bootstrap 4.5 table update

--- js/index.js
+++ js/index.js
@@ -66,30 +66,30 @@ $('#job-03').click(function(){
             $('#chevron-down-03').toggle();
 });
 
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+//   const burger = document.querySelector('.burger');
+//   const nav = document.querySelector('.nav-links');
+//   const navLinks = document.querySelectorAll('.nav-links li');
 
-    //Toggle Nav
-  burger.addEventListener('click',() => {
-      nav.classList.toggle('nav-active');
+//     //Toggle Nav
+//   burger.addEventListener('click',() => {
+//       nav.classList.toggle('nav-active');
 
-    //Animate Links
-  navLinks.forEach((link, index) => {
+//     //Animate Links
+//   navLinks.forEach((link, index) => {
 
-    if(link.style.animation){
-        link.style.animation = '';
-    }else{
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s` 
-        }
-    });
-    //Burger Animation
-    burger.classList.toggle('toggle');
-});
+//     if(link.style.animation){
+//         link.style.animation = '';
+//     }else{
+//         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s` 
+//         }
+//     });
+//     //Burger Animation
+//     burger.classList.toggle('toggle');
+// });
 
-}
-navSlide();
+// }
+// navSlide();
 
 });
 
